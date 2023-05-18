42e621620ebb1f7e0b809863c152fedf267823da
Xannetz Suficiencia <xvsuficiencia@gmail.com>
2023-05-18 16:24:39 +0800

    Added Register and Login

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,12 +1,22 @@
 import { NavLink } from 'react-router-dom';
+import '../style/StyleSheet.css';
 
 function login() {
     return (
-        <>
-            <div>
-                <NavLink to="/"><button>CANCEL</button></NavLink>
+        <div class="logPage">
+            <div class="form">
+                <div class="loginTextboxArea">
+                    <input type="text" placeholder="Username / E-mail Address" class="textbox" />
+                    <input type="text" placeholder="Password" class="textbox" />
+                    <input type="text" placeholder="Confirm Password" class="textbox" />
+                </div>
+                <div class="loginButtons">
+                    <NavLink to="/"><button class="loginButtons">SUBMIT</button></NavLink>
+                    <NavLink to="/"><button class="loginButtons">CANCEL</button></NavLink>
+                </div>
             </div>
-        </>
+            <div class="regPageBG" />
+        </div>
     );
 }
 
--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,12 +1,25 @@
 import { NavLink } from 'react-router-dom';
+import '../style/StyleSheet.css';
 
 function register() {
     return (
-        <>
-            <div>
-                <NavLink to="/"><button>CANCEL</button></NavLink>
+        <div class="regPage">
+            <div class="form">
+                <div class="textboxArea">
+                    <input type="text" placeholder="First Name" class="textbox" />
+                    <input type="text" placeholder="Last Name" class="textbox" />
+                    <input type="text" placeholder="Username" class="textbox" />
+                    <input type="text" placeholder="E-mail Address" class="textbox" />
+                    <input type="text" placeholder="Password" class="textbox" />
+                    <input type="text" placeholder="Confirm Password" class="textbox" />
+                </div>
+                <div class="registerButtons">
+                    <NavLink to="/"><button class="registerButtons">SUBMIT</button></NavLink>
+                    <NavLink to="/"><button class="registerButtons">CANCEL</button></NavLink>
+                </div>
             </div>
-        </>
+            <div class="regPageBG" />
+        </div>
     );
 }
 
